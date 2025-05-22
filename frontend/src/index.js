a79bbe138e50523dfb9b50c8b28c456d9089877e
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-22 13:19:26 +1200

    components folder created, bootstrap and bootswatch applied.
    Header, Footer.
    bootstrap, bootswatch applied,

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './bootstrap.min.css' // Bootstrap import
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
