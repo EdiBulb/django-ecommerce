bf86b57d0d386cb0a51c70a9228cc3e1526c0dbb
Harry <89003206+EdiBulb@users.noreply.github.com>
2025-05-23 18:10:35 +1200

    Implement React Router
    Use "BrowserRouter" and "Link to" for not loading new pages, for
    SPA(Single Page Application)

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,22 +1,32 @@
 import React from 'react'
-import {Navbar, Nav, Container, Row} from 'react-bootstrap'
+import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
     <header>
-    <Navbar bg= "dark" variant='dark' expand="lg" collapseOnSelect >
+      <Navbar bg="dark" variant="dark" expand="lg" collapseOnSelect>
         <Container>
-            <Navbar.Brand href="/">Proshop</Navbar.Brand>
-            <Navbar.Toggle aria-controls="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
+          {/* Brand도 Link로 연결 */}
+          <Navbar.Brand as={Link} to="/">
+            Proshop
+          </Navbar.Brand>
+
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="mr-auto">
-                <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>Cart</Nav.Link>
-                <Nav.Link href="/login"><i className='fas fa-user'></i>Login</Nav.Link>
-                
+              {/* Cart 링크 */}
+              <Nav.Link as={Link} to="/cart">
+                <i className="fas fa-shopping-cart" /> Cart
+              </Nav.Link>
+              {/* Login 링크 */}
+              <Nav.Link as={Link} to="/login">
+                <i className="fas fa-user" /> Login
+              </Nav.Link>
             </Nav>
-            </Navbar.Collapse>
+          </Navbar.Collapse>
         </Container>
-    </Navbar>
+      </Navbar>
     </header>
   )
 }
